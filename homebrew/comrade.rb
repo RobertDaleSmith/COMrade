2ef43a72842244b4090cae4251b09980b9e46d79
Robert Dale Smith <robert@robertdalesmith.com>
2026-05-30 02:55:21 -0500

    Fill in CLI formula sha256 for v0.1.0 source tarball

--- homebrew/comrade.rb
+++ homebrew/comrade.rb
@@ -2,7 +2,7 @@ class Comrade < Formula
   desc "Modern serial, HID, and BLE device monitor for hardware hackers"
   homepage "https://github.com/RobertDaleSmith/COMrade"
   url "https://github.com/RobertDaleSmith/COMrade/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "33d1b1715ddec6d5db071bd3712b972b07a6875c4f65412b41320508d1919b93"
   license "MIT"
 
   depends_on "rust" => :build
