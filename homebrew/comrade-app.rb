a82aeb4422f1770a882efcb6833a58e2b604e880
Robert Dale Smith <robert@robertdalesmith.com>
2026-05-30 02:53:59 -0500

    Make COMrade cask universal (aarch64 + x86_64)
    Use Homebrew's arch helper so Intel Macs get the x64 DMG and Apple
    Silicon Macs get the aarch64 DMG. Previously the cask was hard-coded
    to aarch64, which would have given Intel users the wrong binary.

--- homebrew/comrade-app.rb
+++ homebrew/comrade-app.rb
@@ -1,8 +1,11 @@
 cask "comrade-app" do
-  version "0.1.0"
-  sha256 "4c4b033324456dc95c004cc201f6d92c5871ae5a31de3350154d86a22961f68a"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://github.com/RobertDaleSmith/COMrade/releases/download/v#{version}/COMrade_#{version}_aarch64.dmg"
+  version "0.1.0"
+  sha256 arm:   "4c4b033324456dc95c004cc201f6d92c5871ae5a31de3350154d86a22961f68a",
+         intel: "86e220167f4936667e112d088294c5867aa9b47bd5141011939dc9c0836ef533"
+
+  url "https://github.com/RobertDaleSmith/COMrade/releases/download/v#{version}/COMrade_#{version}_#{arch}.dmg"
   name "COMrade"
   desc "Serial, HID, and BLE device monitor"
   homepage "https://github.com/RobertDaleSmith/COMrade"
